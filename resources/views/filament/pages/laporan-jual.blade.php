3b34d09cbd5f96ca7c569138ce1ae61604f393d4
Muhammadwinata <ilhambobi96@gmail.com>
2024-12-23 08:00:16 +0700

    small update

--- resources/views/filament/pages/laporan-jual.blade.php
+++ resources/views/filament/pages/laporan-jual.blade.php
@@ -5,7 +5,6 @@
         <div class="text-center mb-4">
             <h1 class="text-2xl font-bold text-gray-800 dark:text-gray-200">Laporan Penjualan Barang UD Usaha Jaya</h1>
             <p class="text-gray-600 dark:text-gray-400">Dibuat pada: {{ now()->format('d-m-Y H:i:s') }}</p>
-            <p class="text-gray-600 dark:text-gray-400">Rentang Tanggal: <span id="date-range">{{ $dateRange ?? 'Semua Tanggal' }}</span></p>
         </div>
         <div class="overflow-x-auto">
             {{ $this->table}}
